b678d5b4586bb429bcfb865519d303f47fa77868
betocostadev <robertomarx@gmail.com>
2020-10-10 18:38:34 -0300

    more tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -20,8 +20,36 @@ const favoriteBlog = (blogs) => {
   return blogs[blog]
 }
 
+const mostBlogs = (blogs) => {
+  const authors = blogs.map(e => e.author)
+  const uniqueAuthors = authors.filter((val, i) => authors.indexOf(val) === i)
+  const authorsAndPosts = uniqueAuthors.map(a => {
+    return { author: a, blogs: authors.filter(n => n === a).length }
+  })
+  return authorsAndPosts.find(b => b.blogs === Math.max(...authorsAndPosts.map(a => a.blogs)))
+}
+
+const mostLikes = (blogs) => {
+  const authors = blogs.map(e => e.author)
+  const uniqueAuthors = authors.filter((val, i) => authors.indexOf(val) === i)
+
+  const findWinner = () => {
+    const authorBlogs = []
+    uniqueAuthors.forEach(aut => {
+      let blog = blogs.filter(a => a.author === aut)
+      authorBlogs.push(blog)
+    })
+    const authorsAndLikes = authorBlogs.map((a, i) => { return { author: a[i].author, likes: totalLikes(a) } })
+    const winner = authorsAndLikes.find(a => a.likes === Math.max(...authorsAndLikes.map(a => a.likes)))
+    return winner
+  }
+  return findWinner()
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
