2459f48e97e2c6f426f3de17320aab00c5cc7aad
betocostadev <robertomarx@gmail.com>
2020-10-21 19:53:53 -0300

    updated controllers

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -44,16 +44,17 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
 
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0
-  }
-
   try {
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+    }
+
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     response.json(updatedBlog)
+
   } catch (error) {
     next(error)
   }
